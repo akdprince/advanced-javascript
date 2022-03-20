1c05161302fc5098e4e9a9e420da02a14cf7f5f1
Anik Kumar Das <akd.prince07@gmail.com>
2022-03-20 23:21:23 +0600

    double and triple equal done

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,9 @@
+const first = 2;
+const second = "2";
+
+if(first === second){ //when it === than check type within value. and when == than check type if different it change the type and check within value.
+    console.log("Condition is true.");
+}
+else{
+    console.log("Condition is False.");
+}
